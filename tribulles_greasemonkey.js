efb42c551a9a445dac5d73288ddc04d2dae4a9b4
Fabien Marteau <fabien.marteau@armadeus.com>
2014-09-14 15:16:25 +0200

    init

---
+++ tribulles_greasemonkey.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name        bdphile
+// @namespace   bdphile
+// @description Ajoute un liens vers tribulles pour avoir la disponibilité en occasion
+// @include     http://www.bdphile.info/bdtheque/series/
+// @version     1
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @grant       none
+// ==/UserScript==
+
+// trouver sur http://www.finalclap.com/faq/257-javascript-supprimer-remplacer-accent
+String.prototype.sansAccent = function(){
+    var accent = [
+        /[\300-\306]/g, /[\340-\346]/g, // A, a
+        /[\310-\313]/g, /[\350-\353]/g, // E, e
+        /[\314-\317]/g, /[\354-\357]/g, // I, i
+        /[\322-\330]/g, /[\362-\370]/g, // O, o
+        /[\331-\334]/g, /[\371-\374]/g, // U, u
+        /[\321]/g, /[\361]/g, // N, n
+        /[\307]/g, /[\347]/g, // C, c
+    ];
+    var noaccent = ['A','a','E','e','I','i','O','o','U','u','N','n','C','c'];
+     
+    var str = this;
+    for(var i = 0; i < accent.length; i++){
+        str = str.replace(accent[i], noaccent[i]);
+    }
+    str = str.replace(/ /g, "-");
+    str = str.replace(/''/g, "-");
+    str = "http://www.canalbd.net/tribulles_catalogue_serieoccas_" + str;
+    return str;
+}
+
+var mytable = document.getElementsByClassName("listing")[0].getElementsByTagName('tbody')[0];
+var teair = mytable.getElementsByTagName('tr');
+
+for(var i=0; i < teair.length; i++)
+{
+    var tede = teair[i].getElementsByTagName('td');
+    tede[0].innerHTML = tede[0].innerHTML + " - (<a href=\"" + tede[0].textContent.sansAccent() + "--Albums\"> Occasion </a>)";
+}
+
