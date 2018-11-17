302f412bf663a28b8845fd7a4a1bbef5184730a1
Fabien Marteau <fabien.marteau@armadeus.com>
2018-11-17 14:42:46 +0100

    photogallery: better presentation of prev/next button

--- photo_gallery.js
+++ photo_gallery.js
@@ -3,10 +3,11 @@
 // @namespace   photogallery
 // @description Adding an arrow when visiualize photo in a list 
 // @include     http://raspberrypi.local:8000/Photos/*.JPG
-// @version     1
+// @version     1.01
 // @require  http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // @grant       none
 // ==/UserScript==
+
 console.log("photogallery run");
 function httpGet(theUrl)
 {
@@ -15,6 +16,17 @@ function httpGet(theUrl)
     xmlHttp.send( null );
     return xmlHttp.responseText;
 }
+
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
 
 var files_list_url = document.URL.split('/').slice(0, -1).join('/')+'/';
 var file_name = document.URL.split('/').pop();
@@ -42,23 +54,27 @@ console.log(photos_names);
 console.log(photos_url);
 
 if(file_found > 0){
+  var prev_div = document.createElement('div')
+  prev_div.id = "idprev";
 	var  elprev= document.createElement('a');
 	//elprev.innerHTML = photos_names[file_found - 1];
   elprev.href = photos_url[file_found - 1];
   elprev.innerHTML = '<< ' + photos_names[file_found - 1];
-	document.body.appendChild(elprev);
+  prev_div.appendChild(elprev)
+	document.body.appendChild(prev_div);
 }
 
-intercal = document.createElement('div')
-intercal.innerHTML = " "
-
-document.body.appendChild(intercal)
 
 if(file_found < photos_names.length-1){
+  var next_div = document.createElement('div')
+  next_div.id = "idnext";
   var elnext = document.createElement('a');
   elnext.href = photos_url[file_found + 1];
   elnext.innerHTML = photos_names[file_found + 1] + ' >>';
-  document.body.appendChild(elnext);
+  next_div.appendChild(elnext);
+  document.body.appendChild(next_div);
 }
 
+addGlobalStyle("#idnext { float:right; background-color:silver;}")
+addGlobalStyle("#idprev { float:left; background-color:silver;}")
 
